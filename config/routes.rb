8c3bca79dadc1777c2dd5a8f160f6af4828f53a9
62111341a <water.melon1341@gmail.com>
2022-02-20 19:47:58 +0900

    ユーザー管理機能７

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
    root to: "items#index"
-   resources :items, only: [:create] do
+   
 
 end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,15 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
+    it 'passwordが半角数字だけでは登録できない' do
+      @user.password = '1234567'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
     it 'passwordが5文字以下では登録できない' do
-      @user.password = '12345'
-      @user.password_confirmation = '12345'
+      @user.password = '1234a'
+      @user.password_confirmation = '1234a'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
@@ -35,7 +41,7 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
       it 'passwordが全角文字では登録できないこと'  do
-        @user.password = 'あああああああ'
+      @user.password = 'あああああああ'
       @user.password_confirmation = 'あああああああ'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
