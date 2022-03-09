92c54ea9f9f3e8f113a2382aa66b1a330b0c4231
62111341a <water.melon1341@gmail.com>
2022-03-09 20:13:35 +0900

    商品詳細表示機能１

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,6 +22,5 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_source_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
+  validates :price, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
 end
