3ac3edb1515e0cb3a4ff387e106b826121de41cd
62111341a <water.melon1341@gmail.com>
2022-04-01 16:33:39 +0900

    商品購入機能６

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,9 @@ class OrdersController < ApplicationController
     end
   
     def create
+      @item_order = ItemOrder.new(item_order_params)
     if @item_order.valid?
+
       Payjp.api_key =  ENV["PAYJP_SECRET_KEY"]  
       Payjp::Charge.create(
         amount: @item.price,
@@ -25,7 +27,7 @@ class OrdersController < ApplicationController
   end
     private
     def set_item
-      @item = Item.find(params[:id])
+      @item = Item.find(params[:item_id])
     end
   def item_order_params
     params.require(:item_order).permit(:post, :shipping_source_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])  
