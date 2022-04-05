6d83841a2b45bf8eacf8c038193b0770137facaa
62111341a <water.melon1341@gmail.com>
2022-04-05 17:59:47 +0900

    設定２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index,:create]
   before_action :set_item, only: [:index, :create]
-    def index
+    def index 
+       redirect_to root_path if current_user.id == @item.user_id
         @item_order = ItemOrder.new
         @item=Item.find(params[:item_id])
         if@item.order.present?
